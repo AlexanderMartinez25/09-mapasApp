9f20720b92fd3e68481a09261d5fba9d1104dd50
Alexander <alexander.martinez@lapatota.cl>
2022-07-19 21:47:46 -0400

    Mostrar mapa en pantalla completa

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  mapboxToken: 'pk.eyJ1IjoiYWxleC05MiIsImEiOiJjbDVzdzM5d3IwNnBpM2lxcXpvejV3enhpIn0.p5OiUYTwAGwTevV_fUy4xA'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mapboxToken: 'pk.eyJ1IjoiYWxleC05MiIsImEiOiJjbDVzdzM5d3IwNnBpM2lxcXpvejV3enhpIn0.p5OiUYTwAGwTevV_fUy4xA'
 };
 
 /*
