9222e6af04a1246cbd4c0ee7de84117585d5400d
Alexander <alexander.martinez@lapatota.cl>
2022-07-20 22:42:01 -0400

    componente minimapa

--- src/app/mapas/components/mini-mapa/mini-mapa.component.ts
+++ src/app/mapas/components/mini-mapa/mini-mapa.component.ts
@@ -1,16 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-mini-mapa',
   templateUrl: './mini-mapa.component.html',
   styles: [
+    `
+      div{
+        width: 100%;
+        height: 150px;
+        margin: 0px
+      }
+    `
   ]
 })
-export class MiniMapaComponent implements OnInit {
+export class MiniMapaComponent implements AfterViewInit {
+
+  // para recibir parametros del padre
+  @Input() lngLat: [number, number] = [0, 0]
+  // para seleccionar elemnto por referencia
+  @ViewChild('mapa') divMapa!: ElementRef;
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngAfterViewInit(): void {
 
+    const mapa = new mapboxgl.Map({
+      //y ahora utilizar el elemento nativo
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.lngLat,
+      zoom: 15,
+      interactive: false
+    });
+
+    new mapboxgl.Marker()
+      .setLngLat(this.lngLat)
+      .addTo(mapa)
+
+  }
 }
